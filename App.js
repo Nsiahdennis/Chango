29e8ed759973cec94db776209635bb59f9815974
Nsiah Kofi <nsiahkofidennis001@gmail.com>
2024-05-26 16:56:59 +0000

    initial commit

--- App.js
+++ App.js
@@ -4,16 +4,24 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      
+      <Text>My Name is Nsiah Kofi Dennis</Text>
+      <Text style={{fontWeight: "bold"}}>My Name is Nsiah Kofi Dennis </Text>
+      
+    
+        
+        
+       
+      <StatusBar style="={{ backgroundColor: 'aqua (#00ffff)'}}" />
+      
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "blue",
     alignItems: 'center',
     justifyContent: 'center',
   },
